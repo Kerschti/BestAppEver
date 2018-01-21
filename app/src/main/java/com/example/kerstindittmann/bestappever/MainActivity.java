c21f207873e6fe99ab132abccd47bb67be01705e
Tanschi <tanja.foertsch@gmail.com>
2018-01-21 16:02:38 +0100

    Testtest

--- app/src/main/java/com/example/kerstindittmann/bestappever/MainActivity.java
+++ app/src/main/java/com/example/kerstindittmann/bestappever/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.kerstindittmann.bestappever;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
