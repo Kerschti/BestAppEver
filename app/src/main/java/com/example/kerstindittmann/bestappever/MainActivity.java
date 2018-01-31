2fdc419565987400cad35c73edb194a8d3b68eb8
Tanschi <tanja.foertsch@gmail.com>
2018-01-31 09:15:12 +0100

    schönes alles

--- app/src/main/java/com/example/kerstindittmann/bestappever/MainActivity.java
+++ app/src/main/java/com/example/kerstindittmann/bestappever/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView test;
     TextView test1;
+    EditText zutat;
     Button loc;
 
 
@@ -19,12 +21,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
     }
 
     public void changeActiv(View view) {
         Intent myIntent = new Intent(view.getContext(), GPSLocation2.class);
         startActivity(myIntent);
     }
+
+
+
 }
