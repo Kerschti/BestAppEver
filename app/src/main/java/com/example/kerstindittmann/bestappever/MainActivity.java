898d0be0f70aae05395b33a86540ed919aa9c0ed
Kerschti <gd_sp@hotmail.de>
2018-01-31 16:16:03 +0100

    Google ist scheiße

--- app/src/main/java/com/example/kerstindittmann/bestappever/MainActivity.java
+++ app/src/main/java/com/example/kerstindittmann/bestappever/MainActivity.java
@@ -1,23 +1,44 @@
 package com.example.kerstindittmann.bestappever;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
+    public static final int REQUEST_CODE = 1;
+
+    
+    
     private GoogleApiClient mGoogleApiClient;
+
+    PlaceDetectionClient mPlaceDetectionClient;
+    Task<PlaceLikelihoodBufferResponse> placeResult;
+
+    private static final String TAG = "PLACE DETECTION";
+
 
     TextView test;
     TextView test1;
@@ -36,6 +57,30 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 .addApi(Places.PLACE_DETECTION_API)
                 .enableAutoManage(this, this)
                 .build();
+        
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+       /* if (checkSelfPermission( Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            Toast.makeText(this, "NO PERMISSION", Toast.LENGTH_LONG);
+            requestPermissions( new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        } else {
+            mPlaceDetectionClient.getCurrentPlace(null);
+        }
+
+        placeResult = mPlaceDetectionClient.getCurrentPlace(null);
+        placeResult.addOnCompleteListener(new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                    Log.i(TAG, String.format("Place '%s' has likelihood: %g",
+                            placeLikelihood.getPlace().getName(),
+                            placeLikelihood.getLikelihood()));
+                }
+                likelyPlaces.release();
+            }
+        });*/
 
     }
 
@@ -48,5 +93,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    public void getLocation(View view) {
+        Toast.makeText(this, ""+placeResult.getResult().toString(), Toast.LENGTH_SHORT).show();
     }
 }
