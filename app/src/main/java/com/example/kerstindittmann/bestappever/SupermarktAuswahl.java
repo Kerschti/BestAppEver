c0d7df2805b51a163f2459fca5e071c4de7a0169
Tanschi <tanja.foertsch@gmail.com>
2018-02-06 15:15:59 +0100

    SamShopping

---
+++ app/src/main/java/com/example/kerstindittmann/bestappever/SupermarktAuswahl.java
@@ -0,0 +1,13 @@
+package com.example.kerstindittmann.bestappever;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SupermarktAuswahl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supermarkt_auswahl);
+    }
+}
