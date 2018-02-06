182dd243bbae87f0f41d34000d694b2f6de453aa
Kerschti <gd_sp@hotmail.de>
2018-02-06 21:55:23 +0100

    SQLi

--- app/src/main/java/com/example/kerstindittmann/bestappever/ListActivity.java
+++ app/src/main/java/com/example/kerstindittmann/bestappever/ListActivity.java
@@ -22,6 +22,8 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import static com.example.kerstindittmann.bestappever.ListenHelper.COL_NAME_ID;
+
 
 public class ListActivity extends AppCompatActivity {
 
@@ -42,7 +44,7 @@ public class ListActivity extends AppCompatActivity {
 
         //Welche Spalte soll ausgegeben werden
         String[] projection = {
-                ListenHelper.COL_NAME_ID,
+                COL_NAME_ID,
                 ListenHelper.COL_NAME_DING
         };
 
@@ -75,8 +77,8 @@ public class ListActivity extends AppCompatActivity {
                 CheckBox box = (CheckBox)view.findViewById(R.id.checkBox2);
                 box.setChecked(true);
                 Log.i("CHECK BOX", "THIS WORKS");
-                //Toast.makeText(ListActivity.this, ""+position, Toast.LENGTH_SHORT).show();
                 positionClick = position;
+                Toast.makeText(ListActivity.this, ""+position, Toast.LENGTH_SHORT).show();
 
             }
 
@@ -93,7 +95,7 @@ public class ListActivity extends AppCompatActivity {
 
             cursor.moveToPosition(positionClick);
             int id = cursor.getInt(0);
-            einkaufsliste.delete(ListenHelper.TABLE_NAME_EINKAUFSLISTE, ListenHelper.COL_NAME_ID+ "= "+id,null );
+            einkaufsliste.delete(ListenHelper.TABLE_NAME_EINKAUFSLISTE, COL_NAME_ID+ "= "+id,null );
         }
         positionClick = -1;
     }
@@ -101,28 +103,29 @@ public class ListActivity extends AppCompatActivity {
 
     public void supermaketList(View view) {
         //Sprung in zweite Activity
-        String[] projection = {
-                ListenHelper.COL_NAME_ID,
-                ListenHelper.COL_NAME_DING
-        };
+//        String[] projection = {
+//                ListenHelper.COL_NAME_ID,
+//                ListenHelper.COL_NAME_DING
+//        };
+//
+//        cursor = einkaufsliste.query(ListenHelper.TABLE_NAME_EINKAUFSLISTE,
+//                projection, "1=1", null, null, null, null);
 
-        cursor = einkaufsliste.query(ListenHelper.TABLE_NAME_EINKAUFSLISTE,
-                projection, "1=1", null, null, null, null);
 
-
-
+        String id2 = "";
         int id = 0;
-        if(positionClick != -1){
+        if(positionClick != -1) {
             cursor.moveToPosition(positionClick);
             id = cursor.getInt(0);
+
+            //Toast.makeText(ListActivity.this, ""+id, Toast.LENGTH_SHORT).show();
+
+            id2 = cursor.getString(cursor.getColumnIndex(COL_NAME_ID)); // id is column name in db
+            //cursor.getString(cursor.getColumnIndex("COL_NAME_DING"));
+            //Toast.makeText(this, projection[0]+projection[1], Toast.LENGTH_LONG).show();
+            Toast.makeText(this, id2, Toast.LENGTH_SHORT).show();
         }
-        //Toast.makeText(ListActivity.this, ""+id, Toast.LENGTH_SHORT).show();
-
-        String id2 = cursor.getString( cursor.getColumnIndex("COL_NAME_ID") ); // id is column name in db
-        //cursor.getString(cursor.getColumnIndex("COL_NAME_DING"));
-        //Toast.makeText(this, projection[0]+projection[1], Toast.LENGTH_LONG).show();
-
-
+        positionClick = -1;
         Intent intent = new Intent();
         intent.putExtra("My_Key", id2);
         intent.setClass(this, SupermarktAuswahl.class);
--- app/src/main/java/com/example/kerstindittmann/bestappever/SupermarktAuswahl.java
+++ app/src/main/java/com/example/kerstindittmann/bestappever/SupermarktAuswahl.java
@@ -18,6 +18,8 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import static com.example.kerstindittmann.bestappever.ListenHelper.COL_NAME_ID;
+
 public class SupermarktAuswahl extends AppCompatActivity {
 
     private SQLiteDatabase einkaufsliste;
@@ -41,9 +43,14 @@ public class SupermarktAuswahl extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supermarkt_auswahl);
 
-        int myVal = getIntent().getExtras().getInt("My_Key");
-        Toast.makeText(SupermarktAuswahl.this, ""+myVal, Toast.LENGTH_SHORT).show();
 
+        String myVal = getIntent().getExtras().getString("My_Key");
+
+        Toast.makeText(SupermarktAuswahl.this, myVal, Toast.LENGTH_SHORT).show();
+        //String myValToString = Integer.toString(myVal);
+
+        final ListenHelper lis = ListenHelper.createInstance(this, "Einkaufsliste.db");
+        einkaufsliste = lis.getReadableDatabase();
 
         supermarketlist = (ListView) findViewById(R.id.supermarktlist);
 
@@ -72,7 +79,40 @@ public class SupermarktAuswahl extends AppCompatActivity {
 
             }
 
-        });}
+        });
+
+/*
+
+        //Welche Spalte soll ausgegeben werden
+        String[] projection = {
+                COL_NAME_ID,
+                ListenHelper.COL_NAME_DING
+        };
+
+
+        //Datenbankabfrage
+        cursor = einkaufsliste.query(ListenHelper.TABLE_NAME_EINKAUFSLISTE,
+                projection, "1=1", null, null, null, null);
+*/
+
+/*        String workAround = "SELECT ding FROM liste WHERE _id = ?";
+        cursor = einkaufsliste.rawQuery(workAround, new String[]{myVal});
+        if (cursor.moveToFirst()){
+            do {
+                // Passing values
+                String ding = cursor.getString(1);
+                Toast.makeText(this, ding, Toast.LENGTH_SHORT).show();
+                // Do something Here with values
+            } while(cursor.moveToNext());
+        }
+        cursor.close();
+
+
+        if(positionClick != -1) {
+            cursor.moveToPosition(Integer.valueOf(myVal));
+        }*/
+
+    }
 
     public void onZuerickClick(View view) {
         finish();
