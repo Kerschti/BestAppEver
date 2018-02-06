599be4d5fa58172d3089caf53039209ca400e009
Kerschti <gd_sp@hotmail.de>
2018-02-06 15:54:35 +0100

    Supermarketstuff

--- app/src/main/java/com/example/kerstindittmann/bestappever/SupermarktAuswahl.java
+++ app/src/main/java/com/example/kerstindittmann/bestappever/SupermarktAuswahl.java
@@ -3,7 +3,11 @@ package com.example.kerstindittmann.bestappever;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
@@ -11,6 +15,7 @@ public class SupermarktAuswahl extends AppCompatActivity {
 
     ListView supermarketlist;
     Cursor cursor;
+    private int positionClick = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +32,23 @@ public class SupermarktAuswahl extends AppCompatActivity {
         ArrayAdapter<String> aa = new ArrayAdapter<String>(this, R.layout.list_item,
                 R.id.lv_zutat, supermarkets);
         supermarketlist.setAdapter(aa);
+
+        supermarketlist.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        supermarketlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            //mit position arbeiten bei loeschen braucht man neuen button
+            //mit position daten loeschen
+            //neue Taste und Auswahl loeschen
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                CheckBox box = (CheckBox)view.findViewById(R.id.checkBox2);
+                box.setChecked(true);
+                Log.i("CHECK BOX", "THIS WORKS");
+                //Toast.makeText(ListActivity.this, ""+position, Toast.LENGTH_SHORT).show();
+                positionClick = position;
+
+            }
+
+        });}
     }
 }
